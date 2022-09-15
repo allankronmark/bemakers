d2806936a2f840f7e48086e3bb6e0f6b42f8a7d9
Allan Kronmark <46899428+allankronmark@users.noreply.github.com>
2022-09-15 15:37:44 +0200

    current public version

---
+++ config.js
@@ -0,0 +1,12 @@
+// Customer per Maker
+
+window._bmMakerName = "Copenhagen Distillery";
+
+window.gaMeasurementId = 'G-3PJ6T3Y941';
+
+var supportedCountries = {
+    se: {url: ""},
+    de: {url: ""},
+    dk: {url: ""},
+    gb: {url: "https://www.amathusdrinks.com/b2c/catalogsearch/result/index/?brand=19017&q=copenhagen+distillery"}
+};
